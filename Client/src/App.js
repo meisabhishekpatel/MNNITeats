0c3c6811fe659cc159a868681b8a396f4d5e9c2a
Abhishek Patel <71618624+meisabhishekpatel@users.noreply.github.com>
2023-06-05 00:16:21 +0530

    Update App.js

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,42 +1,16 @@
 // import logo from './logo.svg';
 import './App.css';
-
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import {Container} from "react-bootstrap";
-import {BrowserRouter, Route, Routes } from "react-router-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import TopBar from './components/TopBar';
 import About from './components/About';
 import Contact from './components/Contact';
-import NavBar from './components/NavBar';
-
+import Policy from './components/Policy';
 import NavBar from './components/NavBar';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <TopBar />
-        <NavBar />
-      </div>
-      <Routes>
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/policy" element={<Policy />} />
-      </Routes>
-    </BrowserRouter>
-
-      <TopBar />
-    </div>
-    <Routes>
-      <Route path = "/about" element={<About/>} />
-      <Route path = "/contact" element={<Contact/>} />
-      <Route path = "/policy" element={<Policy/>} />
-    </Routes>
-    </BrowserRouter>
-    
-    
-=======
         <TopBar />
         <NavBar />
       </div>
