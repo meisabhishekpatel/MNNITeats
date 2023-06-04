b323afae0970322badf92c9088a6f8ce09b7734b
Abhishek Patel <ap9506332677@gmail.com>
2023-06-04 23:49:58 +0530

    tweaks

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,36 +1,16 @@
 // import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
-import {Container} from "react-bootstrap";
-import {BrowserRouter, Route, Routes } from "react-router-dom";
-=======
 import { BrowserRouter, Route, Routes } from "react-router-dom";
->>>>>>> c0081b5 (tweaks)
 import TopBar from './components/TopBar';
 import About from './components/About';
 import Contact from './components/Contact';
 import Policy from './components/Policy';
-<<<<<<< HEAD
-=======
 import NavBar from './components/NavBar';
->>>>>>> c0081b5 (tweaks)
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-<<<<<<< HEAD
-      <TopBar />
-    </div>
-    <Routes>
-      <Route path = "/about" element={<About/>} />
-      <Route path = "/contact" element={<Contact/>} />
-      <Route path = "/policy" element={<Policy/>} />
-    </Routes>
-    </BrowserRouter>
-    
-    
-=======
         <TopBar />
         <NavBar />
       </div>
@@ -42,7 +22,6 @@ function App() {
     </BrowserRouter>
 
 
->>>>>>> c0081b5 (tweaks)
   );
 }
 
