565f4298a70fd0002294beade675f86a1cd5e3e1
Nishika <nishika.abhangi@gmail.com>
2023-06-08 19:40:11 +0530

    Deleting an item feature implemented

--- Client/src/actions/cartAction.js
+++ Client/src/actions/cartAction.js
@@ -9,6 +9,21 @@ export const addToCart = (pizza, quantity, variant) => (dispatch, getState) => {
         prices: pizza.prices,
         price: pizza.prices[0][variant] * quantity,
     };
+    if(cartItem.quantity>10){
+        alert("You can add only 10 pizzas at a time");
+    }
+    else{
+    if(cartItem.quantity < 1){
+        dispatch({type:"DELETE_FROM_CART",payload:pizza}) ;
+    }
+    else{
     dispatch({ type: "ADD_TO_CART", payload: cartItem });
-    localStorage.setItem('cartItems', JSON.stringify(getState().cartReducer.cartItems))
+    localStorage.setItem('cartItems', JSON.stringify(getState().cartReducer.cartItems));}
+}};
+
+export const deleteFromCart = (pizza) => (dispatch, getState) => {
+    dispatch({type:'DELETE_FROM_CART',payload:pizza})
+    const cartItems =getState().cartReducer.cartitems; 
+    localStorage.setItem('cartItems',JSON.stringify(cartItems))
+   
 }
--- Client/src/screens/CartScreen.jsx
+++ Client/src/screens/CartScreen.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
-import {Container,Row,Col} from 'react-bootstrap'
-import { useSelector } from 'react-redux'
-import {FaMinusSquare,FaPlusSquare} from 'react-icons/fa'
+import {Container,Row,Col,Button} from 'react-bootstrap'
+import { useSelector,useDispatch } from 'react-redux'
+import {FaMinusSquare,FaPlusSquare,FaTrash} from 'react-icons/fa'
+import { addToCart,deleteFromCart } from '../actions/cartAction'
+
 
 const CartScreen = () => {
     const cartState = useSelector(state => state.cartReducer)
     const cartItems = cartState.cartItems
+    const dispatch = useDispatch();
+    const subTotal=cartItems.reduce((x,item) => x+item.price,0)
   return (
     <>
     <Container>
@@ -20,18 +24,33 @@ const CartScreen = () => {
                             <h5>{item.name} [{item.variant}]</h5>
                             <h6>
                                 {" "}
-                                Price : {item.quantity} X {item.prices[0][item.variant]} ={" "}{item.price}
+                                {/* Price : {item.quantity} X {item.prices[0][item.variant]} ={" "}{item.price} */}
                             </h6>
                             <h6>Quantity :&nbsp;
                             
-                                <FaMinusSquare className="text-danger"/>&nbsp;
+                                <FaMinusSquare 
+                                className="text-danger"
+                                style={{ cursor: "pointer" }}
+                                onClick={() => { dispatch(addToCart(item, item.quantity - 1, item.variant)) }}
+                            />&nbsp;
+
                                 {item.quantity}&nbsp;
-                                <FaPlusSquare className="text-success"/></h6>
+                                <FaPlusSquare 
+                                className="text-success"
+                                tyle={{ cursor: "pointer" }}
+                                onClick={() => { dispatch(addToCart(item, item.quantity + 1, item.variant)) }}
+                                />
+                             </h6>
                                 
                             </Col>
                             <Col md={5}>
                                 <img alt={item.name} src={item.image} style={{width:'150px',height:'80px'}} />
-                            </Col>
+                                <FaTrash className="text-danger"
+                                 style={{cursor:"pointer",marginLeft:"20px" }}
+                                 onClick={() =>{
+                                    dispatch(deleteFromCart(item))}}/>
+                                        </Col>
+                            
                             <hr />
                             </>   
                         ))
@@ -40,6 +59,9 @@ const CartScreen = () => {
             </Col>
             <Col md={4}>
                 <h1>Payment</h1>
+                <h4>SubTotal:</h4>
+                     <h4>RS {subTotal} </h4>
+                     <Button>Checkout</Button>
             </Col>
         </Row>
     </Container>
