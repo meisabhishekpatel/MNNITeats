0d7097fd41b1619270682df94c9819ed316d4d66
Nishika <nishika.abhangi@gmail.com>
2023-07-08 11:52:48 +0530

    Final touches

--- Client/src/pizzadata.js
+++ Client/src/pizzadata.js
@@ -1,92 +1,91 @@
 const pizzas = [
     {
-        name: "Margherita",
-        variants: ["Small", "Medium", "Large"],
+        name: "Aloo-paratha",
+        variants: [ "Medium"],
         prices: [
             {
-                Small: 99,
-                Medium: 199,
-                Large: 399,
+                Small: 45,
+               
             },
         ],
         category: "veg",
-        image: "/images/margherita.jpg",
-        description: "Classic delight with 100% real mozzarella cheese",
+        image: "/images/Aloo-Paratha-4.jpg",
+        description: "Classic paratha with 100% wheat flour",
     },
     {
-        name: "Farmhouse",
+        name: "BoiledEggs",
         variants: ["Small", "Medium", "large"],
         prices: [
             {
-                Small: 229,
-                Medium: 399,
-                Large: 599,
+                Small: 30,
+                Medium: 45,
+                Large: 55,
+            },
+        ],
+        category: "nonveg",
+        image: "/images/boiledegg.jpg",
+        description:
+            "Healthy option for protein lovers",
+    },
+    {
+        name: "Coffee",
+        variants: ["small", "medium", "large"],
+        prices: [
+            {
+                small: 10,
+                medium: 15,
+                large: 25,
+            },
+        ],
+        category: "Drink",
+        description:
+            "Caters your caffeine needs for sure",
+        image: "/images/coffee.jpg",
+    },
+    {
+        name: "Sandwich",
+        variants: ["small", "medium", "large"],
+        prices: [
+            {
+                small: 35,
+                medium: 50,
+                large: 75,
             },
         ],
         category: "veg",
-        image: "/images/farmhouse.jpg",
+        image: "/images/sandwich.jpg",
         description:
-            "Delightful combination of onion, capsicum, tomato & grilled mushroom",
+            "Quick snack to diminish your hunger",
     },
     {
-        name: "Veggie Paradise",
+        name: "Omlette",
         variants: ["small", "medium", "large"],
         prices: [
             {
-                small: 180,
-                medium: 290,
-                large: 460,
-            },
-        ],
-        category: "veg",
-        description:
-            "The awesome foursome! Golden corn, black olives, capsicum, red paprika",
-        image: "/images/veggie_paradise.jpg",
-    },
-    {
-        name: "Chicken Golden Delight",
-        variants: ["small", "medium", "large"],
-        prices: [
-            {
-                small: 249,
-                medium: 349,
-                large: 599,
+                small: 30,
+                medium: 45,
+                large: 60,
             },
         ],
         category: "nonveg",
-        image: "/images/chicken_golden_delight.jpg",
+        image: "/images/omlette.jpg",
         description:
-            "Double pepper barbecue chicken, golden corn and extra cheese, true delight",
+            "A healthy and fulfilling option to grab",
     },
     {
-        name: "Chicken Pepperoni",
+        name: "Tea",
         variants: ["small", "medium", "large"],
         prices: [
             {
-                small: 320,
-                medium: 580,
-                large: 800,
+                small: 10,
+                medium: 15,
+                large: 20,
             },
         ],
-        category: "nonveg",
-        image: "/images/cheesepepperoni.jpg",
+        category: "Drink",
+        image: "/images/tea",
         description:
-            "A classic American taste! Relish the delectable flavor of Chicken Pepperoni, topped with extra cheese",
-    },
-    {
-        name: "Indi Chicken Tikka",
-        variants: ["small", "medium", "large"],
-        prices: [
-            {
-                small: 250,
-                medium: 380,
-                large: 500,
-            },
-        ],
-        category: "nonveg",
-        image: "/images/IndianTandooriChickenTikka.jpg",
-        description:
-            "The wholesome flavour of tandoori masala with Chicken tikka, onion, red paprika & mint mayo",
+            "Freshen up your mood with a sip of tea",
     },
 ];
 export default pizzas;
